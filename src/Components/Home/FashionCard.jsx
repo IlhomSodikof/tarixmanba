0b93e2fdeaa73915f6f475ba8a0395bf9de308b1
Rustamov Nodir <nodirr5555@gmail.com>
2024-03-20 11:34:55 +0500

    yengi manba

---
+++ src/Components/Home/FashionCard.jsx
@@ -0,0 +1,94 @@
+import React from "react";
+
+export default function FashionCard() {
+  return (
+    <>
+      <div className="fashion__container">
+        <div className="fashion__content">
+          <div class="card">
+            <div class="face front">
+              <img
+                src="https://htmldemo.net/pharaoh/pharaoh/images/event/1.jpg"
+                alt=""
+              />
+              <h3>Japan</h3>
+            </div>
+            <div class="face back">
+              <h3>Japan</h3>
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius
+                harum molestiae iste, nihil doloribus fugiat distinctio ducimus
+                maxime totam nulla fuga odio non aperiam eos?
+              </p>
+              <div class="link">
+                <a href="#">Details</a>
+              </div>
+            </div>
+          </div>
+
+          <div class="card">
+            <div class="face front">
+              <img
+                src="https://htmldemo.net/pharaoh/pharaoh/images/event/2.jpg"
+                alt=""
+              />
+              <h3>Spain</h3>
+            </div>
+            <div class="face back">
+              <h3>Spain</h3>
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius
+                harum molestiae iste, nihil doloribus fugiat distinctio ducimus
+                maxime totam nulla fuga odio non aperiam eos?
+              </p>
+              <div class="link">
+                <a href="#">Details</a>
+              </div>
+            </div>
+          </div>
+
+          <div class="card">
+            <div class="face front">
+              <img
+                src="https://htmldemo.net/pharaoh/pharaoh/images/event/1.jpg"
+                alt=""
+              />
+              <h3>Perú</h3>
+            </div>
+            <div class="face back">
+              <h3>Perú</h3>
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius
+                harum molestiae iste, nihil doloribus fugiat distinctio ducimus
+                maxime totam nulla fuga odio non aperiam eos?
+              </p>
+              <div class="link">
+                <a href="#">Details</a>
+              </div>
+            </div>
+          </div>
+          <div class="card">
+            <div class="face front">
+              <img
+                src="https://htmldemo.net/pharaoh/pharaoh/images/event/1.jpg"
+                alt=""
+              />
+              <h3>Perú</h3>
+            </div>
+            <div class="face back">
+              <h3>Perú</h3>
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius
+                harum molestiae iste, nihil doloribus fugiat distinctio ducimus
+                maxime totam nulla fuga odio non aperiam eos?
+              </p>
+              <div class="link">
+                <a href="#">Details</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
--- src/Page/Home.jsx
+++ src/Page/Home.jsx
@@ -3,11 +3,13 @@ import HomeCardMatbuot from "../Components/Home/HomeCardsMatbuot";
 import HomeCardMuxr from "../Components/Home/HomeCardsMuxr";
 import HomeCardArchi from "../Components/Home/HomeCardsArchi";
 import ViewKop from "../Components/Home/ViewKop";
+import FashionCard from "../Components/Home/FashionCard";
 
 export default function Home() {
   return (
     <>
       <main className="card__container">
+        <FashionCard />
         <HomeCardMuxr />
         <HomeCardMatbuot />
         <HomeCardArchi />
