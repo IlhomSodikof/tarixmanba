2618a295a3c7ce09f24acf16d0ddc15ad7a9c110
unknown <behzodrasulov432@gmail.com>
2024-04-19 20:09:58 +0500

    bugs

--- src/Components/Home/Map.jsx
+++ src/Components/Home/Map.jsx
@@ -11,23 +11,25 @@ import { CiInboxIn } from "react-icons/ci";
 import { ImNewspaper } from "react-icons/im";
 import { FaPhotoFilm } from "react-icons/fa6";
 import { Link } from "react-router-dom";
-import { useState } from "react";
-import markerIconSVG from "../../assets/marker.svg";
+import { useCallback, useEffect, useMemo, useState } from "react";
+import markerIconarchialogy from "../../assets/img/archialogy.svg";
 import markerIconPng from "../../assets/img/coin (4) 1.svg";
-
+import markerIcongroup from "../../assets/img/group.svg";
+const leftBarTypeList = {
+  archialogy: markerIconarchialogy,
+  group: markerIcongroup,
+};
 export default function Map() {
   const position = [41.304987, 69.283384];
   const position1 = [41.488801, 69.5857077];
-
   const [num, setNum] = useState(0);
-
+  const [leftBarType, setLeftBarType] = useState(markerIconPng);
   const cordinat = [
     [
       {
         id: 1,
         lan: [41.304987, 69.283384],
         title: "Tarix ddsdf",
-
       },
       { id: 2, lan: [41.488801, 69.5857077], title: "Chirchiq unversity" },
     ],
@@ -56,21 +58,17 @@ export default function Map() {
       { id: 4, lan: [41.430995, 69.490318], title: "Beruniy" },
     ],
   ];
-
-  console.log(cordinat);
-
   const getColor = (value) => {
     return value > 100
       ? "#800026"
       : value > 50
-        ? "#BD0026"
-        : value > 20
-          ? "#E31A1C"
-          : value > 10
-            ? "#FC4E2A"
-            : "#FFEDA0";
+      ? "#BD0026"
+      : value > 20
+      ? "#E31A1C"
+      : value > 10
+      ? "#FC4E2A"
+      : "#FFEDA0";
   };
-
   const style = (feature) => {
     // Assuming your GeoJSON properties have a "value" property
     const value = feature.properties.value;
@@ -138,39 +136,45 @@ export default function Map() {
       click: zoomToFeature,
     });
   };
-  const markerIcon = new L.Icon({
-    iconUrl: markerIconPng,
-    iconRetinaUrl: markerIconPng,
-    popupAnchor: [-0, -0],
-    iconSize: [32, 45],
-  });
+  const markerIcon = useCallback(() => {
+    console.log(leftBarType);
+    return new L.Icon({
+      iconUrl: leftBarType,
+      iconRetinaUrl: leftBarType,
+      popupAnchor: [-0, -0],
+      iconSize: [32, 45],
+    });
+  }, [leftBarType]);
 
-
+  const setMarker = (type) => {
+    if (leftBarType === markerIconarchialogy) {
+      setNum(3);
+    }
+    if (leftBarType === markerIconPng) {
+      setNum(0);
+    }
+    if (leftBarType === markerIcongroup) {
+      setNum(1);
+    }
+    setLeftBarType(type);
+    markerIcon();
+  };
   return (
     <div className="map-full">
       <motion.ul className="map-menu">
-        <motion.li
-          className="map-item"
-          whileHover={{ x: 55 }}
-          onClick={() => setNum(0)}
-        >
+        <motion.li className="map-item" whileHover={{ x: 55 }}>
           <Link className="map-link">
-            {" "}
-            <button>
+            <button onClick={() => setMarker(leftBarTypeList.archialogy)}>
               <TbBuildingCastle />
               <span className="hover-toltip">Arxealogiya</span>
             </button>{" "}
             <span> Arxealogiya</span>
           </Link>
         </motion.li>
-        <motion.li
-          className="map-item"
-          whileHover={{ x: 55 }}
-          onClick={() => setNum(5)}
-        >
+        <motion.li className="map-item" whileHover={{ x: 55 }}>
           <Link className="map-link">
             {" "}
-            <button>
+            <button onClick={() => setMarker(leftBarTypeList.group)}>
               <HiUserGroup />
               <span className="hover-toltip">Xalq og'zaki ijodi</span>
             </button>{" "}
@@ -187,7 +191,6 @@ export default function Map() {
             <button>
               <MdOutlineHistoryEdu />
               <span className="hover-toltip">Qo'lyozmalar</span>
-
             </button>{" "}
             <span> Qo'lyozmalar</span>
           </Link>
@@ -198,11 +201,9 @@ export default function Map() {
           onClick={() => setNum(3)}
         >
           <Link className="map-link">
-            {" "}
             <button>
               <GiTwoCoins />
               <span className="hover-toltip">Tangalar</span>
-
             </button>{" "}
             <span> Tangalar</span>
           </Link>
@@ -217,7 +218,6 @@ export default function Map() {
             <button>
               <RiFilePaper2Line />
               <span className="hover-toltip">Bitiklar</span>
-
             </button>{" "}
             <span> Bitiklar</span>
           </Link>
@@ -233,7 +233,6 @@ export default function Map() {
             <button>
               <LiaBoxOpenSolid />
               <span className="hover-toltip">Tarixiy hujatla</span>
-
             </button>{" "}
             <span> Tarixiy hujatlar</span>
           </Link>
@@ -250,7 +249,6 @@ export default function Map() {
               {" "}
               <GiStamper />
               <span className="hover-toltip">San'at asarlari</span>
-
             </button>
             <span> San'at asarlari</span>
           </Link>
@@ -261,7 +259,6 @@ export default function Map() {
             <button>
               <CiInboxIn />
               <span className="hover-toltip">Arxiv jamg'armalari</span>
-
             </button>{" "}
             <span>Arxiv jamg'armalari</span>
           </Link>
@@ -272,7 +269,6 @@ export default function Map() {
             <button>
               <ImNewspaper />
               <span className="hover-toltip">Matbuot</span>
-
             </button>{" "}
             <span> Matbuot</span>
           </Link>
@@ -283,7 +279,6 @@ export default function Map() {
             <button>
               <FaPhotoFilm />
               <span className="hover-toltip">Foto va video manbalar</span>
-
             </button>
             <span> Foto va video manbalar</span>
           </Link>
@@ -291,6 +286,7 @@ export default function Map() {
       </motion.ul>
 
       <MapContainer
+        key={leftBarType}
         className="map"
         center={position}
         center2={position1}
@@ -308,7 +304,12 @@ export default function Map() {
           onEachFeature={onEachFeature}
         />
         {cordinat[num]?.map((e) => (
-          <Marker key={e.id} position={e.lan} icon={markerIcon}>
+          <Marker
+            key={e.id + num + leftBarType}
+            position={e.lan}
+            icon={markerIcon()}
+          >
+            {console.log(markerIcon())}
             <Popup className="popapa">
               {e.title} <br /> {e.title2}
               <button className="popapa-btn">Batafsil</button>
