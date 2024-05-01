09d85c683f0ec0c062f8e871f121987d3c41439b
Rustamov Nodir <nodirr5555@gmail.com>
2024-05-01 09:36:41 +0500

    asas

--- src/Components/Home/Acardion.jsx
+++ src/Components/Home/Acardion.jsx
@@ -56,23 +56,23 @@ export default function Acardion() {
           TARIXY OBIDALAR VA NODIR tOPILMALAR
         </motion.h1> */}
         <div class="flex-container">
-          <div className="flex-slide acc_img1">
+          <div className="flex-slide acc_img7">
             <div className="flex-title">Tarixiy hujjatlar</div>
           </div>
 
-          <div className="flex-slide acc_img2">
+          <div className="flex-slide acc_img8">
             <div className="flex-title">Muhrlar</div>
           </div>
-          <div className="flex-slide acc_img3">
+          <div className="flex-slide acc_img9">
             <div className="flex-title">San'at asarlari</div>
           </div>
-          <div className="flex-slide acc_img4">
+          <div className="flex-slide acc_img10">
             <div className="flex-title">Matbuat</div>
           </div>
-          <div className="flex-slide acc_img5">
+          <div className="flex-slide acc_img11">
             <div className="flex-title">Arxiv hujjatlari</div>
           </div>
-          <div className="flex-slide acc_img6">
+          <div className="flex-slide acc_img12">
             <div className="flex-title">Ko'r, eshit, tingla</div>
           </div>
         </div>
