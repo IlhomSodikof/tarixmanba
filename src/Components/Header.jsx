54f462636147cc6e76b144c2bc6697bf13f9d7be
Rustamov Nodir <nodirr5555@gmail.com>
2024-05-08 16:42:27 +0500

    ffffff

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -145,6 +145,14 @@ export default function Header() {
             </div>
             <div className="live__calendar">
               <ReactDatePicker />
+              <div>
+                {new Intl.DateTimeFormat("en-TN-u-ca-islamic", {
+                  day: "numeric",
+                  month: "long",
+                  weekday: "long",
+                  year: "numeric",
+                }).format(Date.now())}
+              </div>
             </div>
           </div>
 
